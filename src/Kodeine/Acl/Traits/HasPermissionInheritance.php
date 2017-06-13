98295000453e7ce0fd2eadcc86936ad7edd046e8
Sztyup <sztyup@gmail.com>
2017-06-13 23:54:05 +0200

    Refactor to collect all permissions in the inheritance tree

--- src/Kodeine/Acl/Traits/HasPermissionInheritance.php
+++ src/Kodeine/Acl/Traits/HasPermissionInheritance.php
@@ -1,6 +1,8 @@
 <?php namespace Kodeine\Acl\Traits;
 
 
+use Illuminate\Support\Arr;
+
 trait HasPermissionInheritance
 {
     protected $cacheInherit;
@@ -17,50 +19,24 @@ trait HasPermissionInheritance
         $rights = [];
         $permissions = $this->permissions;
 
-        // ntfs permissions
-        // determine if ntfs is enabled
-        // then more permissive wins
-        $tmp = [];
-        $letNtfs = function ($alias, $slug) use (&$tmp) {
-            if ( config('acl.most_permissive_wins', false) ) {
-                $ntfs[$alias] = array_diff($slug, [false]);
-                if ( sizeof($ntfs) > 0 ) {
-                    $tmp = array_replace_recursive($tmp, $ntfs);
-                }
-            }
-        };
-
         foreach ($permissions as $row) {
 
             // permissions without inherit ids
             if ( is_null($row->inherit_id) || ! $row->inherit_id ) {
-
-                // ntfs determination
-                $letNtfs($row->name, $row->slug);
-
-                // merge permissions
-                $rights = array_replace_recursive($rights, [$row->name => $row->slug], $tmp);
                 continue;
             }
 
             // process inherit_id recursively
-            $inherited = $this->getRecursiveInherit($row->inherit_id, $row->slug);
+            $inherited = array_merge($this->getRecursiveInherit($row->inherit_id, $row->slug));
             $merge = $permissions->where('name', $row->name);
             $merge = method_exists($merge, 'pluck') ? $merge->pluck('slug', 'name') : $merge->lists('slug', 'name');
 
-                // fix for l5.1 and backward compatibility.
+            // fix for l5.1 and backward compatibility.
             // lists() method should return as an array.
             $merge = $this->collectionAsArray($merge);
 
             // replace and merge permissions
             $rights = array_replace_recursive($rights, $inherited, $merge);
-
-            // make sure we don't unset if
-            // inherited & slave permission
-            // have same names
-            if ( key($inherited) != $row->name )
-                unset($rights[$row->name]);
-
         }
 
         return $rights;
@@ -73,54 +49,25 @@ trait HasPermissionInheritance
      */
     protected function getRecursiveInherit($inherit_id, $permissions)
     {
-        // avoid infinite loops,
-        // save ids temporarily.
-        $avoid[] = $inherit_id;
-
-        // ntfs permissions
-        // determine if ntfs is enabled
-        // then more permissive wins
-        $tmp = [];
-        $letNtfs = function ($slug) use (&$tmp) {
-            if ( config('acl.most_permissive_wins', false) ) {
-                $ntfs = array_diff($slug, [false]);
-                if ( sizeof($ntfs) > 0 ) {
-                    $tmp = array_replace($tmp, $ntfs);
-                }
-            }
-        };
-
         // get from cache or sql.
         $inherit = $this->getInherit($inherit_id);
 
+        $inherits = [$inherit];
+
         if ( $inherit->exists ) {
 
-            // ntfs determination
-            $letNtfs($inherit->slug);
-
-            // replace and merge initial permission
-            $permissions = array_replace_recursive($inherit->slug, $permissions, $tmp);
-
             // follow along into deeper inherited permissions recursively
-            while ($inherit && $inherit->inherit_id > 0 && ! is_null($inherit->inherit_id)) {
+            while($inherit && $inherit->inherit_id > 0 && ! is_null($inherit->inherit_id)) {
 
                 // get inherit permission from cache or sql.
                 $inherit = $this->getInherit($inherit->inherit_id);
-
-                // ntfs determination
-                $letNtfs($inherit->slug);
-
-                // replace and merge permissions
-                $permissions = array_replace_recursive($inherit->slug, $permissions, $tmp);
-
-                // avoid getting into infinite loop
-                $avoid[] = $inherit->id;
-                if ( in_array($inherit->inherit_id, $avoid) ) {
-                    break;
-                }
+                $inherits = Arr::prepend($inherits, $inherit);
             };
 
-            return [$inherit->name => $permissions];
+            $inherits = Arr::pluck($inherits, "name");
+            $inherits = array_fill_keys($inherits, ["" => true]);
+
+            return array_merge([$inherit->name => $permissions], $inherits);
         }
 
         return $permissions;
