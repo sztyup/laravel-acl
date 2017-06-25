680954777446a20876b1cbc3daeab9e2282f649d
Sztyup <sztyup@gmail.com>
2017-06-25 23:14:24 +0200

    Remove unnecessary field from DB and make it static

--- src/Kodeine/Acl/Models/Eloquent/Permission.php
+++ src/Kodeine/Acl/Models/Eloquent/Permission.php
@@ -9,7 +9,9 @@ class Permission extends Model
      *
      * @var array
      */
-    protected $fillable = ['name', 'slug', 'description', 'inherit_id'];
+    protected $fillable = ['name', 'description', 'inherit_id'];
+
+    protected $appends = ['slug'];
 
     /**
      * The database table used by the model.
@@ -46,7 +48,7 @@ class Permission extends Model
      */
     public function getSlugAttribute($value)
     {
-        return json_decode($value, true);
+        return ["" => true];
     }
 
     /**
@@ -54,27 +56,7 @@ class Permission extends Model
      */
     public function setSlugAttribute($value)
     {
-        // if nothing being set, clear slug
-        if (empty($value)) {
-            $this->attributes['slug'] = '[]';
-            return ;
-        }
 
-        $value = is_array($value) ? $value : [$value => true];
-
-        // if attribute is being updated.
-        if ( isset($this->original['slug']) ) {
-            $value = $value + json_decode($this->original['slug'], true);
-
-            // sort by key
-            ksort($value);
-        }
-
-        // remove null values.
-        $value = array_filter($value, 'is_bool');
-
-        // store as json.
-        $this->attributes['slug'] = json_encode($value);
     }
 
 }
